b0e37b248008c2c8d2aae791a92e1a764bde6cc3
hange3514 <hange3514@users.noreply.github.com>
2026-07-20 20:09:43 +0800

    chore: EPG默认地址改为内网 xmltv

--- app/src/main/java/top/yogiczy/mytv/data/utils/Constants.kt
+++ app/src/main/java/top/yogiczy/mytv/data/utils/Constants.kt
@@ -27,7 +27,7 @@ object Constants {
     /**
      * 节目单XML地址
      */
-    const val EPG_XML_URL = "http://epg.51zmt.top:8000/e.xml.gz"
+    const val EPG_XML_URL = "http://192.168.0.110:8088/xmltv.xml"
 
     /**
      * 节目单刷新时间阈值（小时）
